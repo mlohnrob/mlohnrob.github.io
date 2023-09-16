26e0e83408c2bf7ef3b5d6714b5afddafc281748
August Emmery-Funch <augustemmeryfunch@gmail.com>
2023-09-16 17:39:08 +0200

    Change branch

--- tina/config.js
+++ tina/config.js
@@ -16,7 +16,7 @@ import {
 
 // Your hosting provider likely exposes this as an environment variable
 // const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-const branch = "master";
+const branch = "github-pages";
 
 export default defineConfig({
   branch,
